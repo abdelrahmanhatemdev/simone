cc4b2ff16fd316c6952ad75c0786d50c119d6241
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-05-21 05:28:08 +0300

    add header search drawer

--- components/modules/header/index.tsx
+++ components/modules/header/index.tsx
@@ -5,17 +5,13 @@ import { IoLockOpen } from "react-icons/io5";
 import dynamic from "next/dynamic";
 
 const Menu = dynamic(() => import("@/components/modules/header/links/Menu"));
+const Search = dynamic(() => import("@/components/modules/header/links/Search"));
 function Header() {
   return (
     <section className="fixed inset-0 z-20 flex justify-between mx-[5vw] md:mx-[8vw] mt-8 h-20">
       <div className="flex gap-5 lg:gap-16 items-center lg:items-end">
         <Menu />
-        <button className="flex gap-4 items-center lg:items-end cursor-pointer">
-          <FaSearch />
-          <span className="uppercase text-neutral-400 tracking-widest text-sm hidden lg:block">
-            Search
-          </span>
-        </button>
+        <Search/>
       </div>
       <div className="flex font-cormorant-garamond text-3xl lg:text-4xl font-bold  items-center lg:items-end leading-6">
         P. Simone
---
+++ components/modules/header/links/Search.tsx
@@ -0,0 +1,44 @@
+import { DrawerClose } from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import dynamic from "next/dynamic";
+const HeaderDrawer = dynamic(() => import("@/components/custom/HeaderDrawer"));
+import { memo } from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaSearch,
+  FaTimes,
+  FaTwitter,
+} from "react-icons/fa";
+
+function Search() {
+  return (
+    <HeaderDrawer
+      title="Menu"
+      trigger={
+        <button className="flex gap-4 items-center lg:items-end cursor-pointer">
+          <FaSearch />
+          <span className="uppercase text-neutral-400 tracking-widest text-sm hidden lg:block">
+            Search
+          </span>
+        </button>
+      }
+    >
+      <div className="flex flex-col gap-10 px-10 lg:px-[20%] py-20 w-full h-[100svh]">
+        <DrawerClose asChild>
+          <div className="flex gap-3 items-center cursor-pointer">
+            <FaTimes />
+            <span className="uppercase text-neutral-400 tracking-wide font-cormorant-garamond">
+              Close
+            </span>
+          </div>
+        </DrawerClose>
+        <input
+          placeholder="Search For Products, Brands And News"
+          className="border-0 text-lg md:text-3xl border-b rounded-none w-full outline-0 py-3 md:py-5 font-cormorant-garamond"
+        />
+      </div>
+    </HeaderDrawer>
+  );
+}
+export default memo(Search);
