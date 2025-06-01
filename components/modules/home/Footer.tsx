fefb429e86be7e3a065b1a1e06886f3d09071789
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-06-01 10:52:24 +0300

    enhanc animation

--- components/modules/home/Footer.tsx
+++ components/modules/home/Footer.tsx
@@ -4,21 +4,24 @@ import dynamic from "next/dynamic";
 import { memo } from "react";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6";
 const Animate = dynamic(() => import("@/components/custom/Animate"));
-const fadeSlideUp = animate({ type: "fadeSlideUp"});
+ 
 const fadeSlideDown = animate({ type: "fadeSlideDown"});
+const fadeSlideDown1 = animate({ type: "fadeSlideDown", delay: .4});
+const fadeSlideDown2 = animate({ type: "fadeSlideDown", delay: .5});
+const fadeSlideDown3 = animate({ type: "fadeSlideDown", delay: .6});
 
 function Footer() {
   return (
     <section className="mx-5 mt-20 mb-40 flex flex-col gap-3" id="contact">
       <div className="flex flex-col gap-5 justify-center items-center">
-        <Animate variants={fadeSlideUp} element="p">Follow Us</Animate>
-        <Animate variants={fadeSlideDown} className="flex gap-2">
+        <Animate variants={fadeSlideDown} element="p">Follow Us</Animate>
+        <Animate variants={fadeSlideDown1} className="flex gap-2">
           <FaFacebookF size={18} />
           <FaInstagram size={18} />
           <FaTwitter size={18} />
         </Animate>
       </div>
-      <Animate variants={fadeSlideUp} className="flex flex-col sm:flex-row gap-3 justify-center items-center text-xs flex-wrap">
+      <Animate variants={fadeSlideDown2} className="flex flex-col sm:flex-row gap-3 justify-center items-center text-xs flex-wrap">
         {contact.map((item, i) => (
           <span
             className={
@@ -31,7 +34,7 @@ function Footer() {
           </span>
         ))}
       </Animate>
-      <Animate variants={fadeSlideDown} className="flex justify-center text-xs">
+      <Animate variants={fadeSlideDown3} className="flex justify-center text-xs">
         Terms. &copy; 2025 Simone USA
       </Animate>
     </section>
--- components/modules/home/News.tsx
+++ components/modules/home/News.tsx
@@ -7,11 +7,10 @@ import { animate } from "@/lib/animation/animate";
 const SectionHeading = dynamic(() => import("@/components/custom/SectionHeading"))
 const Animate = dynamic(() => import("@/components/custom/Animate"))
 
-const fadeSlideUp = animate({type:"fadeSlideUp"})
-const fadeSlideUp1 = animate({type:"fadeSlideUp", duration: 1})
-const fade = animate({type:"fade", duration: 2, delay: .2})
-const fade1 = animate({type:"fade", duration: 2, delay: .2})
-
+const fadeSlideDown = animate({ type: "fadeSlideDown"});
+const fadeSlideDown1 = animate({ type: "fadeSlideDown", delay: .4});
+const fadeSlideDown2 = animate({ type: "fadeSlideDown", delay: .5});
+const fadeSlideDown3 = animate({ type: "fadeSlideDown", delay: .6});
 function News() {
   return (
     <section className="mt-40 md:mt-[35vw] mx-5 flex flex-col items-end gap-10 md:gap-40" id="news">
@@ -19,14 +18,14 @@ function News() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 md:mx-[15vw]">
         {news.map((item, i) => (
           <article key={i} className="flex flex-col gap-8">
-            <Animate variants={fadeSlideUp} className="text-4xl">{item.title}</Animate>
+            <Animate variants={fadeSlideDown} className="text-4xl">{item.title}</Animate>
             <div className="flex flex-col gap-2">
-              <Animate variants={fadeSlideUp1} element="p" className="text-neutral-400 tracking-wider text-xl">
+              <Animate variants={fadeSlideDown1} element="p" className="text-neutral-400 tracking-wider text-xl">
                 {item.author}, {item.date}
               </Animate>
-              <Animate variants={fade}  className="leading-6 md:leading-8 xl:leading-10">{item.description}</Animate>
+              <Animate variants={fadeSlideDown2}  className="leading-6 md:leading-8 xl:leading-10">{item.description}</Animate>
             </div>
-            <Animate variants={fade1}>
+            <Animate variants={fadeSlideDown3}>
 
             <a href="#">Read more</a>
             </Animate>
